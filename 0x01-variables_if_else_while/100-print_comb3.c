0e6d40faff6368f506a0d31796a4880ab07bd2e7
Daniel Anah <1192daniel@gmail.com>
2022-04-09 07:02:55 -0700

    loo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+/*
+* main - print combination of two digits
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int degit1, degit2;
+
+	for (degit1 = 0; degit1 < 9; degit1++)
+	{
+		for (degit2 = degit1 + 1; degit2 <= 9; degit2++)
+		{
+			putchar((degit1 % 10) + '0');
+			putchar((degit2 % 10) + '0');
+
+			if (degit1 == 8 && degit2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
