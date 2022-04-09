d0133a4ed22ae281ec02c6c5b1ad48ad992dbfa7
Daniel Anah <1192daniel@gmail.com>
2022-04-09 14:54:54 -0700

    looi

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,23 +8,30 @@
 */
 int main(void)
 {
-	int digit1, digit2;
+	int digit1, digit2, digit3, digit4;
 
-	for (digit1 = 0; digit1 <= 98; digit1++)
+	for (digit1 = 0; digit1 <= 9; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 <= 99; digit2++)
+		for (digit2 = 0; digit2 <= 8; digit2++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit1 % 10) + '0');
-			putchar(' ');
-			putchar((digit2 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+			for (digit3 = 0; digit3 <= 9; digit3++)
+			{
+				for (digit4 = 1; digit4 <= 9; digit4++)
+				{
 
-			if (digit1 == 98 && digit2 == 99)
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar(' ');
+				putchar((digit3 % 10) + '0');
+				putchar((digit4 % 10) + '0');
+
+			if (digit1 == 9 && digit2 == 8 && digit3 == 9 && digit4 == 9)
 				continue;
 
 			putchar(',');
 			putchar(' ');
+				}
+			}
 		}
 	}
 
