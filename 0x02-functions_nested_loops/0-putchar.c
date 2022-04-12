600534f4ecfc0fbee3b7c28f97bbe4c4fa25d1ee
Daniel Anah <1192daniel@gmail.com>
2022-04-12 10:48:10 -0700

    lol

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 /**
 * main - prints _putchar followed by new line
 *
-* Return: Always (0) Success
+* Return:nothing, return void
 */
 int main(void)
 {
@@ -14,6 +14,7 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
+_putchar('\n');
 
 return (0);
 }
