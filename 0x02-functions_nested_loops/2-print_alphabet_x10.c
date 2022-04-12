af97c3584964ed30e3f3141bb7a296ecf48eca86
Daniel Anah <1192daniel@gmail.com>
2022-04-12 15:16:36 -0700

    #function to print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+*print-alphabetx10 - prints alphabets ten times
+*
+* Return: Always 0
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	i = 0;
+	c = 'a';
+
+	while (i <= 10)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		i++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
