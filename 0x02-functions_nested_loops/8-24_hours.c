70e33c7134760f4ec21d0b8974bfb667bc65e2c3
Daniel Anah <1192daniel@gmail.com>
2022-04-13 16:53:58 -0700

    ##function to print every hour and minute in a day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,6 +15,8 @@ while (hours < 24)
 	minutes = 0;
 	while (minutes < 60)
 	{
+		_putchar((hours / 10) + '0');
+		_putchar((hours % 10) + '0');
 		_putchar(':');
 		_putchar((minutes / 10) + '0');
 		_putchar((minutes % 10) + '0');
@@ -22,8 +24,6 @@ while (hours < 24)
 		minutes++;
 
 	}
-	_putchar((hours / 10) + '0');
-	_putchar((hours % 10) + '0');
 	hours++;
 }
 }
