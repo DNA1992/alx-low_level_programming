78ec47c137221a26dc1a43bce36351f2680c5bc8
Daniel Anah <1192daniel@gmail.com>
2022-04-12 17:40:29 -0700

    lol

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
 */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
 	{
 		return (1);
 	}
@@ -14,22 +14,4 @@ int _isalpha(int c)
 	{
 		return (0);
 	}
-}
-int main(void)
-
-{
-    int r;
-
- r = _isalpha('H');
- _putchar(r + '0');
- r = _isalpha('o');
- _putchar(r + '0');
- r = _isalpha(108);
-
- _putchar(r + '0'); 
- r = _isalpha(';');
- _putchar(r + '0');
- _putchar('\n');
-return (0);
-
 }
