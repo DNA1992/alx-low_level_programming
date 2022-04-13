192afed182b90494ba72805b3bf3a7a88636eb85
Daniel Anah <1192daniel@gmail.com>
2022-04-13 06:52:19 -0700

    ###function to print sign of numbers and zero if zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_sign - prints sign of number
+* Return: 1 if number is positve
+* 0 if zero
+* -1 if negative
+*/
+int print_sign(int n);
+{
+	int n;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
