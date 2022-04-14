055baafc95b3de7310b52497ea05dd0c1be8f228
Daniel Anah <1192daniel@gmail.com>
2022-04-14 09:00:34 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,13 +9,11 @@ void times_table(void)
 	int x;
 	int y;
 	int z;
-
 	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
 			z = x * y;
-
 			if ((z / 10) == 0)
 			{
 				if (y == 0)
@@ -33,18 +31,18 @@ void times_table(void)
 						_putchar(' ');
 					}
 				}
-			else
-			{
+				else
+				{
 				_putchar((z / 10) + '0');
 				_putchar((z % 10) + '0');
-				
 				if (y < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
 				}
+				}
 			}
-		}
 		_putchar('\n');
+		}
 	}
 }
