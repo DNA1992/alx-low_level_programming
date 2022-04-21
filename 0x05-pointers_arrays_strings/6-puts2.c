8bede2e41420d7b26df8697896fc4fb517c2f37a
Daniel Anah <1192daniel@gmail.com>
2022-04-21 11:54:01 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* puts2 - prints other characters of a string starting from first
+* @str: string to  use
+*/
+void puts2(char *str);
+{
+	int i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(*str);
+			str++;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
