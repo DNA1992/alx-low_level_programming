6bf06c513b55dcf1e99b8b7c3c39b925ef3452bc
Daniel Anah <1192daniel@gmail.com>
2022-04-21 11:18:17 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* rev_string - to reverse a string
+*
+* @s: pointer type
+*/
+void rev_string(char *s)
+{
+	int p1, p2;
+	char str[900];
+
+	p1 = 0;
+	while (*s != '\0')
+	{
+		str[p1] = *s;
+		p1++;
+		s++;
+	}
+	s--;
+
+	p2 = 0;
+	while (p2 != p1)
+	{
+		*s = p2;
+		p2++;
+		s--;
+	}
+	s++;
+}
