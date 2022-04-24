4b291660f125f44a7f6a864faaabccdc0aef4a62
Daniel Anah <1192daniel@gmail.com>
2022-04-24 15:29:10 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * _strcat - function to concatnate two strings
@@ -11,14 +12,15 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (i = 0; *dest != '\0'; i++)
 	{
 	}
 	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
+		*dest = src[j];
 		dest++;
 	}
-	dest[i] = '\0';
+	*dest = '\0';
+	dest -= (i + j);
 	return (dest);
 }
