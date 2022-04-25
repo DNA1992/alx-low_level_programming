bd949b61238d2457e30b4a9c50e42d0ff717faab
Daniel Anah <1192daniel@gmail.com>
2022-04-25 06:20:57 -0700

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* reverse_array - reverses an array of number
+*@a: array 
+*@n: number
+*/
+
+void reverse_array(int *a, int n)
+{
+	int x, y, z;
+
+	for (x = 0, y = n - 1; x < n / 2; x++)
+	{
+		z = a[x];
+		a[x] = a[y];
+		a[y] = z;
+		y--;
+	}
+}
