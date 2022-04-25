2c785bc54ca4b60fc3b2933437e6a7603c662392
Daniel Anah <1192daniel@gmail.com>
2022-04-25 08:27:41 -0700

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* cap_string - capitalises all words of a string
+*@a:pointer for type char
+*
+* Return: string
+*/
+char *cap_string(char *a);
+{
+	char separator[] = " \t\n,;.!?\"(){}";
+	int x, y;
+
+	for (x = 0; a[x] != '\0')
+	{
+		if (a[x] >= 'a' && a[x] <= 'z')
+		{
+			if (x == 0)
+			{
+				a[x] -= 32;
+			}
+		else
+		{
+			for (y = 0; separator[y] != '\0'; y++)
+			{
+				if (a[x - 1] == separator[y])
+				{
+					a[x] -= 32;
+				}
+			}
+		}
+
+		}
+	}
+	return (a);
+}
