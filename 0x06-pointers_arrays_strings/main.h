626a7b7125070f1858c42a77ad1e193737f4c079
Daniel Anah <1192daniel@gmail.com>
2022-04-25 04:49:11 -0700

    0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,1 @@
-#ifndef strcat functions
 char *_strncat(char *dest, char *src, int n);
-int _putchar(char c);  /* An example function declaration */
-char *_strcat(char *dest, char *src);
-char *_strncpy(char *dest, char *src, int n);
-#endif
