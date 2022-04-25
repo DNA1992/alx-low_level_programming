2c78b3c400cf19fd462bdbb9ea74f6f5c298ff6a
Daniel Anah <1192daniel@gmail.com>
2022-04-25 06:43:33 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*string_toupper - converts lowercase of string to uppercase
+@s:sring to use
+* Return: Converted string
+*/
+
+char *string_toupper(char *)
+{
+	for (int x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] >= 97 && s[x] <= 122)
+		{
+			s[x] -= 32;
+		}
+	}
+	return (s);
+}
