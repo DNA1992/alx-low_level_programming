895b0806892123b7e0dd42498dd6565a318dedcd
Daniel Anah <1192daniel@gmail.com>
2022-04-28 10:53:51 -0700

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,20 +2,21 @@
 
 /**
 * _strlen_recursion - outputs length of string
-*@s: string 
+*@s:string
 * Return: string
 */
 
 int _strlen_recursion(char *s)
 {
 	int length;
+
 	length = 0;
 
 	if (*s == '\0')
 	{
 		return (0);
 	}
-	else 
+	else
 	{
 		length = 1 + _strlen_recursion(s + 1);
 		return (length);
