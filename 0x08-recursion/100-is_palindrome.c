ca273309eba60be62d81e704d0a87c9a5c253e5e
Daniel Anah <1192daniel@gmail.com>
2022-04-29 13:54:25 -0700

    pali

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+*palindrom1 -set true or false for palindrome string
+*@x:string
+*@y:length of string
+* Return: 1 on success
+*error -1 if not thru
+*/
+
+int palindrom1(char *x, int y)
+{
+	if (*x == 0)
+		return (y - 1);
+	return (palindrom1(a + 1, y + 1));
+}
+
+/**
+*palindrom2 - string reverse
+*@x:string
+*@y:length of string
+* Return: 1 on success
+*error -1 if not true
+*/
+
+int palindrom2(char *x, int y)
+{
+	if (*x != *(x + 1))
+		return (0);
+	else if (*x == 0)
+		return (1);
+	return (palindrom2(x + 1, y - 2));
+}
+
+/**
+* is_palindrome - checks if a string is palindrome
+*@s: pointer to string
+*Return:on success 1
+*error -1
+*/
+
+int is_palindrome(char *s)
+{
+	int y;
+
+	y = palindrom1(s, 0);
+	return (palindrom2(s, y));
+}
