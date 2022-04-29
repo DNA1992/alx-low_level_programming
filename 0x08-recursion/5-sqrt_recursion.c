da5c0577e55cc08afac36745ceda71ea332f6131
Daniel Anah <1192daniel@gmail.com>
2022-04-29 10:47:53 -0700

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+*sqrt - evaluates square root of numbers
+*@x: same value as n
+*@y: number that iterates from 1 to n
+*Return: 1 on success
+*error print -1
+*/
+
+int sqrt(int x, int y)
+{
+	if (y * y == x)
+		return (y);
+
+	if (y * y > x)
+		return (-1);
+	return (sqrt(x, y + 1));
+}
+
+/**
+* _sqrt_recursion - returns the square root of natural numbers
+*@n: number to evaluate square root
+* Return: 0 success
+*error -1  for error
+*/
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n, 1));
+}
+
