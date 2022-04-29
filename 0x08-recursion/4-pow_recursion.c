20debbd1b42073f4f8478d857f909eaaa4148fb9
Daniel Anah <1192daniel@gmail.com>
2022-04-29 09:44:44 -0700

    4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _pow_recursion - raises x to y
+*@x:Number Integer
+*@y:power
+*
+*Return: 1 for success
+*error it returns -1
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
