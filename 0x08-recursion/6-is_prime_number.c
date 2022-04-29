df2fc4203bb36ff458517a66f3b872f832f78b7a
Daniel Anah <1192daniel@gmail.com>
2022-04-29 11:37:50 -0700

    6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* primeNum - determines a prime number
+*@x:same as n
+*@y:natural number
+* Return: 1 on success
+*error gives 0
+*/
+
+int primeNum(int x, int y)
+{
+	if (x == y)
+		return (1);
+	else if (x % y == 0)
+		return (0);
+	return (primeNum(x, y + 1));
+}
+
+/**
+* _is_prime_number - checks prime number
+*@n:natural number
+* Return: 1 success
+* error -1
+*/
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	return (primeNum(n, 2));
+}
