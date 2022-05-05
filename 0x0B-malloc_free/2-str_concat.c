cb304cac0512d982fce5fc928f2ee13c0c71d603
Daniel Anah <1192daniel@gmail.com>
2022-05-05 09:55:42 -0700

    2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* str_concat - fills memory with constant byte
+* @s1:first string
+* @s2:second string
+* Return:an address
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	int x, size1, size2;
+	char *s;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		s1 = "";
+		s2 = "";
+	}
+
+	for (size1 = 0; s1[size1] != '\0'; size1++)
+		;
+	for (size2 = 0; s2[size2] != '\0'; size2++)
+		;
+	s = malloc((sizeof(char) * size1) + (sizeof(char) * size2) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	for (x = 0; x < (size1 + size2); x++)
+	{
+		if (x >= size1)
+			s[x] = s2[x - size1];
+		else
+			s[x] = s1[x];
+	}
+	s[x] = '\0';
+	return (s);
+}
