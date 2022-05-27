3edac658c229d0701ebc3c74bb39726e92a0a889
Daniel Anah <1192daniel@gmail.com>
2022-05-27 07:57:41 -0700

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*main - print name of this function
+*@argc:length ofarguments
+*@argv:argument
+*Return: 0 if successful
+*/
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		printf("%s\n", *argv);
+	}
+	return (0);
+}
