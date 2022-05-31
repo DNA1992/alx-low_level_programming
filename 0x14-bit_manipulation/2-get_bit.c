bfa03177aaaeacfa4e7cec54a4382ab2472db563
Daniel Anah <1192daniel@gmail.com>
2022-05-31 15:39:27 -0700

    output

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*get_bit - program to get bit value at any interval
+*@n:size of pointer
+*@index: there
+*Return: integer
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index < 63)
+		return (-1);
+	return (((n) & ((1) << (index))) ? 1 : 0);
+}
