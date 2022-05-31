126cbecbe0a6eb927cdf6089241fb248207759f9
Daniel Anah <1192daniel@gmail.com>
2022-05-31 16:30:22 -0700

    output clear

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -3,13 +3,13 @@
 *clear_bit - program to clear bit of number to zero
 *@n:pointer size
 *@index:index
-*Return: integer
+*Return:integer
 */
 int clear_bit(unsigned long int *n, unsigned int index);
 {
 	if (index > 63)
 		return (-1);
 
-	*n &= (~(1 << index));
+	*n &= ~(1 << index);
 	return (1);
 }
