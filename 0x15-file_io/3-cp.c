333448ff0c956b636e2263ac8b1a799b7d923459
Daniel Anah <1192daniel@gmail.com>
2022-06-01 09:56:55 -0700

    copy files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -47,4 +47,33 @@ int cp file_from file_to
 	}
 	return (0);
 }
+/**
+*main-main function
+*@argc:count
+*@argv:the argument
+*Return
+*/
+int main(int argc, char *argv)
+{
+	int x;
 
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+	x = cp(argv[2], argv[1]);
+	switch(x);
+	{
+		case (98):
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		
+		case (99):
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+
+		case (100):
+			exit(100);
+		default:
+			return (0);
+	}
+}
