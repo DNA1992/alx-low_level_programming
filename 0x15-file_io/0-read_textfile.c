b296ed02a92b6eeeea7187602737c93c3000b775
Daniel Anah <1192daniel@gmail.com>
2022-06-01 05:39:04 -0700

    read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,37 +7,31 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd = 0;
-	ssize_t lettercompa = 0, x = 0;
-	void *buffer;
+	int fd, rd, wd;
+	char *buff = malloc(sizeof(char) * letters);
 
 	if (filename == NULL)
-		return (0);
-
-	fd = open(filename, O_RDONLY, 0600);
-	if (fd == -1)
-		return (0);
-
-	buffer = malloc(letters);
-	if (buffer == NULL)
-		return (0);
-
-	lettercompa = read(pd, buffer, letters)
-		if (lettercompa == -1)
-		{
-			free(buffer);
-			close(fd);
-			return (0);
-		}
-
-	x = write(STDOUT_FILENO, buffer, lettercompa);
-	if (x == -1 || x != lettercompa)
 	{
-		free(buffer);
-		close(fd);
 		return (0);
 	}
-	free(buffer);
+
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	rd = read(fd, buff, letters);
+	if (rd == -1)
+	{
+		return (0);
+	}
+	wd = write(STDOUT_FILENO, buff, rd);
+	if (wd == -1)
+	{
+		return (0);
+	}
 	close(fd);
-	return (x);
+	free(buff);
+	return (wd);
 }
