d49437ccd74ae7e88703d4b9cfee0fe49ec87652
Daniel Anah <1192daniel@gmail.com>
2022-06-01 06:10:55 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 /**
 *create_file-create a file
 *@filename:file to be created
-*text_content- content of file
+*@text_content- content of file
 *Return:1 on success, -1 on error
 */
 int create_file(const char *filename, char *text_content)
