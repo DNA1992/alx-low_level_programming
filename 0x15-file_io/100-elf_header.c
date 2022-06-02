8c5864ec2207b342fa9ab908e3a60f3e0a239f81
Daniel Anah <1192daniel@gmail.com>
2022-06-02 16:49:37 -0700

    elf header

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*main-fills memory
+*@argc:pointer size
+*@argv:index
+*Return:0 success
+*/
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	return (0);
+}
